df102c9583f6efb89dc3d4b2a0776af3298b5f53
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2025-01-24 07:10:36 -0500

    Agregar archivos de proyecto.

---
+++ ExamenP3HerreraDilan/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenP3HerreraDilan
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenP3HerreraDilan/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamenP3HerreraDilan
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

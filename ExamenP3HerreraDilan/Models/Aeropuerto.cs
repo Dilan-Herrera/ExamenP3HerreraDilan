0b7a8d3f9c9a28182f61df95b93cb3cf1d83a182
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2025-01-24 07:30:12 -0500

    Creacion de las carpetas y el modelo para el consumo de la API

---
+++ ExamenP3HerreraDilan/Models/Aeropuerto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenP3HerreraDilan.Models
+{
+    public class Aeropuerto
+    {
+        public string Pais { get; set; }
+        public Ubicacion Ubicacion { get; set; }
+        public Contacto Contacto { get; set; }
+    }
+
+    public class Ubicacion
+    {
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+    }
+
+    public class Contacto
+    {
+        public string Correo { get; set; }
+    }
+}
---
+++ ExamenP3HerreraDilan/ViewModels/BuscarAeropuertoDHerrera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenP3HerreraDilan.ViewModels
+{
+    class BuscarAeropuertoDHerrera : INotifyPropertyChanged
+    {
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+}
